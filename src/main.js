3b3e45d30f43e09cf5dba26edc83ce5e9059612c
Yichuan Shen <shen.yichuan@gmail.com>
2018-10-25 21:27:33 +0200

    Add nameMove()

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const {mod, equals, hasVertex} = require('./helper')
+const {mod, equals, hasVertex, getNeighbors, getLiberties, getUnnamedHoshis} = require('./helper')
 
 exports.shapeLibrary = require('../data/shapes.json')
 
@@ -82,6 +82,68 @@ exports.shapeMatch = function(data, [x, y], shape) {
 
         if (i >= 0) return [i, sign !== as]
     }
+
+    return null
+}
+
+exports.nameMove = function(data, sign, [x, y], {shapes = null} = {}) {
+    let height = data.length
+    let width = data.length === 0 ? 0 : data[0].length
+    let oldSign = data[y][x]
+
+    if (oldSign !== 0 || !hasVertex([x, y], width, height)) return 'Pass'
+    if (shapes == null) shapes = exports.shapeLibrary
+
+    let neighbors = getNeighbors([x, y], width, height)
+
+    // Check suicide
+
+    data[y][x] = sign
+
+    if (getLiberties([x, y], data).length === 0) {
+        data[y][x] = oldSign
+        return 'Suicide'
+    }
+
+    data[y][x] = oldSign
+
+    // Check atari & capture
+
+    for (let [nx, ny] of neighbors) {
+        if (data[ny][nx] === -sign) continue
+
+        let libertyCount = getLiberties([nx, ny], data).length
+        if (libertyCount === 1) return 'Take'
+        if (libertyCount === 2) return 'Atari'
+    }
+
+    // Check connection
+
+    let friendlies = neighbors.filter(([nx, ny]) => data[ny][nx] === sign)
+    if (friendlies.length === neighbors.length) return 'Fill'
+    if (friendlies.length >= 2) return 'Connect'
+
+    // Match shape
+
+    data[y][x] = sign
+
+    for (let shape of shapes) {
+        if (exports.shapeMatch(data, [x, y], shape)) {
+            data[y][x] = oldSign
+            return shape.name
+        }
+    }
+
+    data[y][x] = oldSign
+
+    // Determine position to edges
+
+    let equalsVertex = equals([x, y])
+    if (equalsVertex([(width - 1) / 2, (height - 1) / 2])) return 'Tengen'
+    if (getUnnamedHoshis().some(equalsVertex)) return 'Hoshi'
+
+    let diff = [x, y].map((z, i) => Math.min(z + 1, [width, height][i] - z)).sort((a, b) => a - b)
+    if (diff[1] <= 6) return diff.join('-') + ' point'
 
     return null
 }
