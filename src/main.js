96b76d8ec00ac4e68ba0860e6aa19a13c4469c25
Yichuan Shen <shen.yichuan@gmail.com>
2019-01-07 11:38:16 +0100

    Fix typo

--- src/main.js
+++ src/main.js
@@ -87,7 +87,7 @@ exports.matchShape = function*(data, anchor, shape) {
             yield {
                 symmetryIndex: i,
                 invert: sign !== as,
-                vertices: vertices.map(([vertex, _]) =>
+                vertices: shape.vertices.map(([vertex, _]) =>
                     getBoardSymmetries(vertex, width, height)[i]
                 )
             }
@@ -95,22 +95,23 @@ exports.matchShape = function*(data, anchor, shape) {
     }
 }
 
-exports.nameMove = function(data, sign, [x, y], {shapes = null} = {}) {
+exports.nameMove = function(data, sign, vertex, {shapes = null} = {}) {
     let height = data.length
     let width = data.length === 0 ? 0 : data[0].length
     let oldSign = data[y][x]
 
     if (oldSign !== 0) return null
-    if (!hasVertex([x, y], width, height)) return 'Pass'
+    if (!hasVertex(vertex, width, height)) return 'Pass'
     if (shapes == null) shapes = require('../data/shapes.json')
 
-    let neighbors = getNeighbors([x, y], width, height)
+    let [x, y] = vertex
+    let neighbors = getNeighbors(vertex, width, height)
 
     // Check suicide
 
     data[y][x] = sign
 
-    if (getLiberties([x, y], data).length === 0) {
+    if (getLiberties(vertex, data).length === 0) {
         data[y][x] = oldSign
         return 'Suicide'
     }
@@ -138,7 +139,7 @@ exports.nameMove = function(data, sign, [x, y], {shapes = null} = {}) {
     data[y][x] = sign
 
     for (let shape of shapes) {
-        for (let _ of exports.matchShape(data, [x, y], shape)) {
+        for (let _ of exports.matchShape(data, vertex, shape)) {
             data[y][x] = oldSign
             return shape.name
         }
@@ -148,11 +149,14 @@ exports.nameMove = function(data, sign, [x, y], {shapes = null} = {}) {
 
     // Determine position to edges
 
-    let equalsVertex = equals([x, y])
+    let equalsVertex = equals(vertex)
     if (equalsVertex([(width - 1) / 2, (height - 1) / 2])) return 'Tengen'
     if (getUnnamedHoshis().some(equalsVertex)) return 'Hoshi'
 
-    let diff = [x, y].map((z, i) => Math.min(z + 1, [width, height][i] - z)).sort((a, b) => a - b)
+    let diff = vertex
+        .map((z, i) => Math.min(z + 1, [width, height][i] - z))
+        .sort((a, b) => a - b)
+
     if (diff[1] <= 6) return diff.join('-') + ' point'
 
     return null
